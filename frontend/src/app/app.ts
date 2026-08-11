5435e588c758225c3a57010326f5cfaf070dc8fc
Pablo01PGL <bluepablo007@gmail.com>
2026-08-11 10:22:11 +0200

    Update Angular 22

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   imports: [],
   templateUrl: './app.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.scss'
 })
 export class App {
